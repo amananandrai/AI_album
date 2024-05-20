90d4e20e429fd74560f38985669bf7aa212db17e
Shubham Kumar <hello@shubhkumar.in>
2024-05-20 17:07:51 +0530

    API ready

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -1,20 +1,23 @@
 import { getImages } from "@/app/services/images";
 import { ImageResponse } from "@/app/types/ExtendNextApiReqeuest";
+import { NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     try {
-        const resp: ImageResponse = await getImages(9, 0);
-
-        console.debug("🚀  file: route.ts:11  GET  resp:", resp);
-
-        return Response.json({ resp })
+        const params = request.nextUrl.searchParams;
+        const limit = parseInt(params.get("limit") || "9");
+        const offset = parseInt(params.get("offset") || "0");
+        const resp: ImageResponse = await getImages(limit, offset);
+        return Response.json({ resp }, {
+            status: 200, headers: {
+                'Cache-Control': 'public, max-age=60, stale-while-revalidate=10'
+            },
+        });
 
     }
     catch (e: any) {
         console.error(e.message);
-        return Response.json({ error: true, message: e.message })
-        // //@ts-ignore next-line
-        // Response.status(500).json({ error: true, message: e.message })
+        return Response.json({ error: true, message: e.message }, { status: 500 })
     }
 
 }

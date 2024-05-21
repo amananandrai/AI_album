c585cb700d23cf43cc20e764f0433d5c8f35db4c
Shubham <hello@shubhkumar.in>
2024-05-21 22:04:37 +0530

    Update imageGallery.tsx

--- src/app/components/imageGallery.tsx
+++ src/app/components/imageGallery.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import Image from "next/image";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import { GalleryContext } from "../context/gallery";
 import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 import { ChevronLeft, ChevronRight } from "lucide-react";
@@ -12,70 +12,103 @@ export function ImageGallery() {
     const { images, totalPages, fetchImages, setImages } = useContext(GalleryContext);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [modalImage, setModalImage] = useState("");
+    const modalRef = useRef(null);
 
     const toggleModal = (src: string) => {
         setIsModalOpen(!isModalOpen);
         setModalImage(src);
     };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setModalImage("");
+    };
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+            handleCloseModal();
+        }
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (modalRef.current && !modalRef.current.contains(event.target)) {
+            handleCloseModal();
+        }
+    };
+
     useEffect(() => {
-        console.log(totalPages)
         fetchImages(page).then((data) => {
             setImages(data.rows);
         });
     }, [page]);
 
+    useEffect(() => {
+        if (isModalOpen) {
+            document.addEventListener('keydown', handleKeyDown);
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isModalOpen]);
+
     if (images.length === 0) {
         return <div>Loading...</div>
     }
+
     return (
         <div className="flex justify-center items-center flex-col">
             <div className="grid gap-0.5  sm:grid-cols-2 md:grid-cols-3 grid-rows-3">
                 {images.map((src, index) => (
-                    <>
-                        <Button key={index}
+                    <div key={index}>
+                        <Button
                             onClick={() => toggleModal(src)}
                             data-modal-target="extralarge-modal" data-modal-toggle="extralarge-modal" className="w-full h-full bg-transparent p-1 m-0">
                             <GalleryImage src={src} loading="eager" fullscreen={false} />
                         </Button>
-                        {isModalOpen && (
-                            <div
-                                id="extralarge-modal"
-                                className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto h-[calc(100%-1rem)] max-h-full"
-                            >
-                                <div className="relative w-full max-w-2xl max-h-auto">
-                                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                                            <button
-                                                type="button"
-                                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                                onClick={() => toggleModal(src)}
-                                            >
-                                                <svg
-                                                    className="w-3 h-3"
-                                                    aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    fill="none"
-                                                    viewBox="0 0 14 14"
-                                                >
-                                                    <path
-                                                        stroke="currentColor"
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        strokeWidth="2"
-                                                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                                                    />
-                                                </svg>
-                                                <span className="sr-only">Close modal</span>
-                                            </button>
-                                        </div>
-                                        <GalleryImage src={modalImage} loading="lazy" fullscreen={true} />
-                                    </div>
-                                </div>
-                            </div>
-                        )}
-                    </>
+                    </div>
                 ))}
-
+                {isModalOpen && (
+                    <div
+                        id="extralarge-modal"
+                        className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto h-[calc(100%-1rem)] max-h-full"
+                    >
+                        <div className="relative w-full max-w-2xl max-h-auto" ref={modalRef}>
+                            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                    <button
+                                        type="button"
+                                        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                        onClick={handleCloseModal}
+                                    >
+                                        <svg
+                                            className="w-3 h-3"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 14 14"
+                                        >
+                                            <path
+                                                stroke="currentColor"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                            />
+                                        </svg>
+                                        <span className="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                                <GalleryImage src={modalImage} loading="lazy" fullscreen={true} />
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
             <div className="flex gap-1">
                 <div className="flex justify-center items-center">
@@ -86,67 +119,34 @@ export function ImageGallery() {
                         <span>Previous</span>
                     </Button>
                 </div>
+                {[...Array(totalPages).keys()].map((pageNumber) => (
+                    <div className="flex justify-center items-center" key={pageNumber}>
+                        <Button variant="ghost"
+                            onClick={() => setPage(pageNumber)}
+                        >
+                            <span>{pageNumber + 1}</span>
+                        </Button>
+                    </div>
+                ))}
                 <div className="flex justify-center items-center">
                     <Button variant="ghost"
-                        onClick={() => setPage(0)}
-                    >
-                        <span>1</span>
-                    </Button>
-                </div>
-                <div className="flex justify-center items-center">
-                    <Button variant="ghost"
-                        onClick={() => setPage(1)}
-                    >
-                        <span>2</span>
-                    </Button>
-                </div>
-                <div className="flex justify-center items-center">
-                    <Button variant="ghost"
-                        onClick={() => setPage(2)}
-                    >
-                        <span>3</span>
-                    </Button>
-                </div>
-                <div className="flex justify-center items-center">
-                    <Button variant="ghost"
-                    >
-                        <span>...</span>
-                    </Button>
-                </div>
-                <div className="flex justify-center items-center">
-                    <Button variant="ghost"
-                        onClick={() => setPage(totalPages - 2)}
-                    >
-                        <span>{totalPages - 1}</span>
-                    </Button>
-                </div>
-                <div className="flex justify-center items-center">
-                    <Button variant="ghost"
-                        onClick={() => setPage(totalPages - 1)}
-                    >
-                        <span>{totalPages}</span>
-                    </Button>
-                </div>
-                <div className="flex justify-center items-center">
-                    <Button variant="ghost"
-                        onClick={() => page < totalPages && setPage(page + 1)}
+                        onClick={() => page < totalPages - 1 && setPage(page + 1)}
                     >
                         <span>Next</span>
                         <ChevronRight className="h-4 w-4" />
                     </Button>
                 </div>
             </div>
-
         </div>
     );
 }
-
 
 type GalleryImageProp = {
     src: string
     loading?: "eager" | "lazy" | undefined
     fullscreen: boolean
 }
+
 const GalleryImage = ({ src, loading, fullscreen }: GalleryImageProp) => {
     return (
         <Image
@@ -163,5 +163,5 @@ const GalleryImage = ({ src, loading, fullscreen }: GalleryImageProp) => {
             width="0"
             height="0"
         />
-    )
+    );
 }

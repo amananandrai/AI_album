c621a6c7684086088fb2f5e8a1171f48b57f51ba
Shubham Kumar <hello@shubhkumar.in>
2024-05-21 13:21:45 +0530

    update

--- src/app/components/imageGallery.tsx
+++ src/app/components/imageGallery.tsx
@@ -10,7 +10,13 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 export function ImageGallery() {
     const [page, setPage] = useState(0);
     const { images, totalPages, fetchImages, setImages } = useContext(GalleryContext);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalImage, setModalImage] = useState("");
 
+    const toggleModal = (src: string) => {
+        setIsModalOpen(!isModalOpen);
+        setModalImage(src);
+    };
     useEffect(() => {
         console.log(totalPages)
         fetchImages(page).then((data) => {
@@ -25,17 +31,66 @@ export function ImageGallery() {
         <div className="flex justify-center items-center flex-col">
             <div className="grid gap-0.5  md:grid-cols-3 grid-rows-3">
                 {images.map((src, index) => (
-                    <Dialog key={index}>
-                        <DialogTrigger asChild>
-                            <Button className="w-full h-full bg-transparent p-1 m-0">
-                                <GalleryImage src={src} loading="eager" fullscreen={false} />
-                            </Button>
-                        </DialogTrigger>
-                        <DialogContent className="max-w-screen max-h-screen w-auto h-auto scale-100 m-4">
-                            <GalleryImage src={src} loading="lazy" fullscreen={true} />
-                        </DialogContent>
-                    </Dialog>
+                    <>
+                        <Button key={index}
+                            onClick={() => toggleModal(src)}
+                            data-modal-target="extralarge-modal" data-modal-toggle="extralarge-modal" className="w-full h-full bg-transparent p-1 m-0">
+                            <GalleryImage src={src} loading="eager" fullscreen={false} />
+                        </Button>
+                        {isModalOpen && (
+                            <div
+                                id="extralarge-modal"
+                                className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto h-[calc(100%-1rem)] max-h-full"
+                            >
+                                <div className="relative w-full max-w-7xl max-h-full">
+                                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                            <button
+                                                type="button"
+                                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                                onClick={toggleModal}
+                                            >
+                                                <svg
+                                                    className="w-3 h-3"
+                                                    aria-hidden="true"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 14 14"
+                                                >
+                                                    <path
+                                                        stroke="currentColor"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                                    />
+                                                </svg>
+                                                <span className="sr-only">Close modal</span>
+                                            </button>
+                                        </div>
+                                        <GalleryImage src={modalImage} loading="lazy" fullscreen={true} />
+                                        {/* <div className="flex items-center p-4 md:p-5 space-x-3 rtl:space-x-reverse border-t border-gray-200 rounded-b dark:border-gray-600">
+                                            <button
+                                                onClick={toggleModal}
+                                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                            >
+                                                I accept
+                                            </button>
+                                            <button
+                                                onClick={toggleModal}
+                                                className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                            >
+                                                Decline
+                                            </button>
+                                        </div> */}
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                        {/* <GalleryImage src={src} loading="lazy" fullscreen={true} /> */}
+                    </>
                 ))}
+
             </div>
             <div className="flex gap-1">
                 <div className="flex justify-center items-center">

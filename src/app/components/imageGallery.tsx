a419233d5af15c2ab9c3b4e0d04174400284a04e
Shubham Kumar <hello@shubhkumar.in>
2024-05-21 18:06:51 +0530

    fix

--- src/app/components/imageGallery.tsx
+++ src/app/components/imageGallery.tsx
@@ -48,7 +48,7 @@ export function ImageGallery() {
                                             <button
                                                 type="button"
                                                 className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                                onClick={toggleModal}
+                                                onClick={() => toggleModal(src)}
                                             >
                                                 <svg
                                                     className="w-3 h-3"

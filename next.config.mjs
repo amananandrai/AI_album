33d8f66c6cc9361cd0287f54daf6c002a04d8f6a
Aman Anand Rai <amananandrai@gmail.com>
2025-07-16 23:07:48 +0530

    Update image domains in next.config.mjs to remove the protocol from Cloudflare R2 storage URL

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ['https://pub-e4f08df325af40158c3446f307ca8267.r2.dev'],
+        domains: ['pub-e4f08df325af40158c3446f307ca8267.r2.dev'],
     },
 };
 

c8e3a886c9ae3eb88b5ac8a49c3cc6e2d2fa848e
Aman Anand Rai <amananandrai@gmail.com>
2025-08-06 21:51:41 +0530

    Refactor next.config.mjs and tsconfig.json for improved configuration clarity. Removed swcMinify option and updated TypeScript library paths and target settings for better compatibility.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
     images: {
         minimumCacheTTL: 86400,
         domains: ['pub-e4f08df325af40158c3446f307ca8267.r2.dev'],
     },
 };
-
-
 
 export default nextConfig;

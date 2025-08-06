24a7e1bcdbe7c392fa6aca328a293da89beb4830
Aman Anand Rai <amananandrai@gmail.com>
2025-08-06 22:04:46 +0530

    Update next.config.mjs to replace image domains with remotePatterns for enhanced image loading configuration and security.

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,14 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         minimumCacheTTL: 86400,
-        domains: ['pub-e4f08df325af40158c3446f307ca8267.r2.dev'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'pub-e4f08df325af40158c3446f307ca8267.r2.dev',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     },
 };
 
